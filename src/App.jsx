96ef849785e7cf516f9428bccf66cc21c4fc4ffa
popmircea27 <pop_mirceastefan@yahoo.ro>
2025-09-29 16:05:11 +0300

    update login

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import { useState } from 'react'
 import Navbar from './components/navBar/NavBar'
 import ApiPlayground from '../tests/APIplayground.test'
+import LoginPage from './admin/login/LoginPage'
 function App() {
   
 
   return (
     <>
       <Navbar/>
+      <LoginPage/>
       {/* <ApiPlayground/> */}
     </>
   )
